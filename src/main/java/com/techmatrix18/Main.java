0dce4a1818b3133188e32189f31de70be7371614
Alexander <alexander@makklays.com.ua>
2025-06-09 19:42:13 +0200

    first commit

--- src/main/java/com/techmatrix18/Main.java
+++ src/main/java/com/techmatrix18/Main.java
@@ -1,4 +1,6 @@
-package com.techmatrix18;public class Main {
+package com.techmatrix18;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
